51729ada8d627246c1e0977282c9c25c5a25f0a1
Shawn <shawn.p.wallis@gmail.com>
2015-02-02 16:22:07 -0500

    cleaned up client

--- client/client.py
+++ client/client.py
@@ -5,34 +5,52 @@ import sys
 ADDRESS = ('localhost', 43650)
 DEFAULT_READ_SIZE = 256
 
-print 'Client instance started.'
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Client socket created.'
-
-try:
-    sock.connect(ADDRESS)
-    print 'Client connected on local port %d.' % sock.getsockname()[1]
-except Exception as e:
-    print 'Connection failed. Error Code : ' + repr(e)
-    sys.exit(1)
-print 'Client connected to server.'
-
-print 'Client sending request.'
-sock.send('hello')
-
-try:
-    print 'Client awaiting response...'
-    response = ''
-    packet = None
-    while packet != '':
-
-        packet = sock.recv(DEFAULT_READ_SIZE)
-        response += packet
+class ClientConnection(object):
+    def __init__(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except Exception as e:
+            print 'Socket failed. Error Code : ' + repr(e)
+        print 'Client socket created.'
+        
+        try:
+            self.socket.connect(ADDRESS)
+            print 'Client connected on local port %d.' % self.socket.getsockname()[1]
+        except Exception as e:
+            print 'Connection failed. Error Code : ' + repr(e)
+        print 'Client connected to server.'
     
-    print 'Client got response:', response
-except Exception as e:
-    print 'err:', repr(e)
-finally:
-    sock.close()
-    print 'Client socket closed.'
-print 'Client stopped.'    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        if self.socket is not None:
+            self.flush()
+            self.socket.close()
+        self.socket = None
+    
+    def flush(self):
+        while self.socket.recv(DEFAULT_READ_SIZE) != '':
+            continue
+    
+    def request(self, req):
+        print 'Sending request:', repr(req)
+        self.socket.send(req)
+        
+        try:
+            print 'Awaiting response...'
+            response = ''
+            packet = None
+            while packet != '':
+                packet = self.socket.recv(DEFAULT_READ_SIZE)
+                response += packet
+            
+            print 'Got response:', repr(response)
+        except Exception as e:
+            print 'err:', repr(e)
+        finally:
+            self.flush()
+        return response
+ 
+with ClientConnection() as client:
+    client.request('hello there')
