d08495feb01071a03a6c612341c52b6ad20c978d
Shawn <shawn.p.wallis@gmail.com>
2015-02-04 11:11:26 -0500

    - fixed id to match server db id - changed from big-endian to network-byte-order

--- client/session.py
+++ client/session.py
@@ -5,7 +5,7 @@ import base64
 
 from client import ClientConnection
 
-user_id = 1433
+user_id = 143
 username = ''
 age = 0
 gps = None
@@ -18,7 +18,7 @@ def decode_response(response):
     return response
 
 with ClientConnection() as client:
-    packFmt = '<HI'
+    packFmt = '!HI'
     request = struct.pack(packFmt, 2, user_id)
     response = client.request(request)
     response = decode_response(response)
@@ -26,14 +26,14 @@ with ClientConnection() as client:
     username = d['username']
 
 with ClientConnection() as client:
-    request = struct.pack('<HI', 3, user_id)
+    request = struct.pack('!HI', 3, user_id)
     response = client.request(request)
     response = decode_response(response)
     d = json.loads(response)
     age = d['age']
 
 with ClientConnection() as client:
-    request = struct.pack('<HI', 4, user_id)
+    request = struct.pack('!HI', 4, user_id)
     response = client.request(request)
     response = decode_response(response)
     d = json.loads(response)
